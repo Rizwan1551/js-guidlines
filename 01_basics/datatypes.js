36e9752a516023029d0ac9140a6e270f4ec23cfe
Rizwan1551 <razi.rizwan304@gmail.com>
2024-05-30 23:12:38 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as new version
+// alert (3 + 3) // we are using nodejs not browser
+
+console.log(3 + 
+    3)// readability should be high
+
+    let name = "Rizwan"
+    let age = 18
+    let isLoggedIn = false
+
+    // number ==> 2 to 53
+    // bigint // for big values and number
+
+    // sting = ""
+    // boolean ==> true/false
+    // null ==> standalone value/value not yet assigned
+
+    // undefind ==>
+    // symbol ==> unique
+    
+    // object
+
+    console.log(typeof null); // object
+    
+
+
